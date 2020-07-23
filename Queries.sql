4a9f830713f4e778c5d9ff2b59ddb4e58ce5e2ae
SunilVeer <62995033+SunilVeer@users.noreply.github.com>
2020-07-24 00:08:02 +1000

    final changes

--- Queries.sql
+++ Queries.sql
@@ -26,12 +26,12 @@ select distinct famname,givename
 from academic
 join interest
 on academic.acnum=interest.acnum
-where lower(interest.DESCRIP) LIKE '%db%'
+where lower(interest.DESCRIP) LIKE '%database%' or lower(interest.DESCRIP) LIKE '%db%'
 order by academic.famname;
 --Q.7
 select famname,givename
 from academic
-where TITLE LIKE '%prof%';
+where lower(TITLE) LIKE '%prof%';
 --Q.8
 select famname,givename from author
 where acnum Not in (select acnum from academic
@@ -47,4 +47,5 @@ where panum>=100 and panum<=200;
 --Q.10
 select dept.deptnum, dept.deptname, ac.acnum, i.fieldnum
 from department dept, academic ac, interest I
-where dept.deptnum=ac.deptnum and ac.acnum=i.acnum;
+where dept.deptnum=ac.deptnum and ac.acnum=i.acnum
+order by dept.deptnum;
