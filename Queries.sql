40217774013991408188fa2e616c7948ae8ab0e8
SunilVeer <62995033+SunilVeer@users.noreply.github.com>
2020-07-23 22:29:37 +1000

    Update Queries.sql
    last five queries added

--- Queries.sql
+++ Queries.sql
@@ -21,3 +21,30 @@ join author au
 on ac.acnum=au.acnum
 group by ac.acnum
 having count(ac.acnum)>=2;
+--Q.6
+select distinct famname,givename
+from academic
+join interest
+on academic.acnum=interest.acnum
+where lower(interest.DESCRIP) LIKE '%db%'
+order by academic.famname;
+--Q.7
+select famname,givename
+from academic
+where TITLE LIKE '%prof%';
+--Q.8
+select famname,givename from author
+where acnum Not in (select acnum from academic
+join interest
+on academic.acnum=interest.acnum
+join field
+on interest.fieldnum=field.fieldnum
+where field.title='Logic Programming')
+--Q.9
+select panum,title
+from paper
+where panum>=100 and panum<=200;
+--Q.10
+select dept.deptnum, dept.deptname, ac.acnum, i.fieldnum
+from department dept, academic ac, interest I
+where dept.deptnum=ac.deptnum and ac.acnum=i.acnum;
